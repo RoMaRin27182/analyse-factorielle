b9953fbe73c06eff331f671358a7e7e6516e7f6b
romarin27182 <eleine.leblanc@gmail.com>
2020-05-13 16:56:50 -0400

    4 premieres questions

--- niveau1/scripts/script.js
+++ niveau1/scripts/script.js
@@ -1,4 +1,23 @@
-$(function(){
-$(".question").hide();
-$(".reponse").hide();
+
+$(function() {
+    $("#commencer.bouton").click(function() {
+        $(this).hide();
+        questions = $(".question").first();
+        questions.css('display', 'block');
+        $(".bouton#repondre").css('display', 'block');
+
+    });
 });
+
+$(function() {
+    $(".bouton#repondre").click(function() {
+        $(this).hide();
+        questions.css('display', 'none');
+        reponses = $(".reponse").first();
+        reponses.css('display', 'block');
+
+    });
+});
+
+/*questions.next().css('display', 'block');
+});*/
