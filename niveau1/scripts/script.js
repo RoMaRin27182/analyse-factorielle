e088517085cd3f28b0c50561ca9d6061494cd188
romarin27182 <eleine.leblanc@gmail.com>
2020-05-15 14:29:24 -0400

    code ok pour une quetion, reste à boucler

--- niveau1/scripts/script.js
+++ niveau1/scripts/script.js
@@ -1,23 +1,48 @@
-
+var scoretotal=0;
 $(function() {
-    $("#commencer.bouton").click(function() {
-        $(this).hide();
-        questions = $(".question").first();
-        questions.css('display', 'block');
-        $(".bouton#repondre").css('display', 'block');
+ $("#commencer.bouton").click(function() {
+  $(this).hide();
+  questions = $(".question").first();
+  questions.css('display', 'block');
+  $(".bouton#repondre").css('display', 'block');
 
-    });
+ });
 });
 
 $(function() {
-    $(".bouton#repondre").click(function() {
-        $(this).hide();
-        questions.css('display', 'none');
-        reponses = $(".reponse").first();
-        reponses.css('display', 'block');
+ $(".bouton#repondre").click(function() {
+  $(this).hide();
+  questions.css('display', 'none');
+  reponses = $(".reponse").first();
+  reponses.css('display', 'block');
+  $(".bouton#ok").css('display', 'block');
 
-    });
+ });
 });
+
+$(function() {
+ $(".bouton#ok").click(function() {
+  $(this).hide();
+  $("#autoeval.container").css('display', 'block');
+  $(".bouton#okeval").css('display', 'block');
+ });
+});
+
+$(function() {
+ $(".bouton#okeval").click(function() {
+  sco = $("#autoevaluation input:checked").val();
+  if (sco) {
+    $(this).hide();
+   sco = Number(sco);
+   scoretotal=scoretotal+sco;
+   $("#evaluation").css('display', 'block');
+   $("#evaluation").html("pointage : " + scoretotal);
+  };
+ });
+});
+
+
+
 
 /*questions.next().css('display', 'block');
 });*/
